1ab3b1fe3f467e063b1d694b4bc487263579ca2c
brent-yoast <brent@yoast.com>
2022-02-18 14:11:51 +0100

    added logging

--- tap_shopify_partners/shopify_partners.py
+++ tap_shopify_partners/shopify_partners.py
@@ -111,7 +111,7 @@ class Shopify(object):  # noqa: WPS230
 
         self.logger.info('````````````````````About to enter date for loop (shopify_partners.py')
         for date_day in self._start_days_till_now(start_date_input):
-            self.logger.info(f'Inside the date for loop and currently looking at {date_day} (shopify_partners.py')
+            self.logger.info(f'~~~~~~~Inside the date for loop and currently looking at {date_day} (shopify_partners.py')
             query: str = QUERIES['transactions']
             # Replace dates in placeholders
             # query = query.replace(':fromdate:', date_day + "T00:00:00.000000Z")
@@ -138,10 +138,10 @@ class Shopify(object):  # noqa: WPS230
            #  print(response_data['data']['transactions']['edges'])
             # self.logger.info('~~~~~~~')
             # self.logger.info(transactions)
-            # self.logger.info(response_data)
+            self.logger.info(f'|||||||||||||||||| Response data: {response_data}')
 
             # yield cleaner(date_day, transactions)
-
+            self.logger.info('==========About to enter the transaction for loop (shopify_partners.py->shopify_partners_transactions')
             for transaction in response_data['data']['transactions']['edges']:
                 self.logger.info('`````````````````````````````````````````')
                 self.logger.info('Before flattening:')
