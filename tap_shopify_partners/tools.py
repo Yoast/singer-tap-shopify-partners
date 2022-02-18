66fdf4a047cd4b116e5bd363d89ffde03579ffac
brent-yoast <brent@yoast.com>
2022-02-18 14:43:01 +0100

    fixed str formatting dates

--- tap_shopify_partners/tools.py
+++ tap_shopify_partners/tools.py
@@ -77,6 +77,6 @@ def create_bookmark(stream_name: str, bookmark_value: str) -> str:
         # Return tomorrow's date
         tomorrow: date = datetime.strptime(
             bookmark_value,
-            '%Y-%m-%dT%H:%M:%fZ',
+            '%Y-%m-%dT%H:%M:%S.%fZ',
         ).date() + timedelta(days=1)
         return tomorrow.isoformat()
