1d26cb24df6c9e5338d8644d1759dd884ecd670d
brent-yoast <brent@yoast.com>
2022-03-18 16:00:17 +0100

    temp logging

--- tap_shopify_partners/sync.py
+++ tap_shopify_partners/sync.py
@@ -85,7 +85,7 @@ def sync_record(stream: CatalogEntry, row: dict, state: dict) -> None:
     # Create new bookmark
     # new_bookmark: str = tools.create_bookmark(stream.tap_stream_id, bookmark)
     LOGGER.info('-------------------------------------------')
-    LOGGER.info(f'^^^^^^ Bookmark: {bookmark}')
+    LOGGER.info(f'^^^^Bookmark: {bookmark}')
 
     # Write a row to the stream
     singer.write_record(
