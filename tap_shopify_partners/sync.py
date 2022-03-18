dc13102e54c3407ba2ba90e13931eaf5f8a9e38e
brent-yoast <brent@yoast.com>
2022-03-18 15:02:48 +0100

    indent error

--- tap_shopify_partners/sync.py
+++ tap_shopify_partners/sync.py
@@ -100,7 +100,7 @@ def sync_record(stream: CatalogEntry, row: dict, state: dict) -> None:
         for key1 in state:
             for key2 in state[key1]:
                 state[key1][key2]['start_date'] = state[key1][key2]['start_date'].replace('000000Z', '100000Z')
-        LOGGER.info(f'```````state: {state}')
+                LOGGER.info(f'```````state: {state}')
         singer.write_bookmark(
             state,
             stream.tap_stream_id,
